5b7635e54f47a54d89bfa6890515de290c1fb957
Nicole <er8015@wayne.edu>
2021-02-02 23:10:07 -0500

    Added patientLog app

--- Treeo/settings.py
+++ Treeo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'patient_log.apps.PatientLogConfig',
     'ReqAppt.apps.ReqapptConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- patient_log/urls.py
+++ patient_log/urls.py
@@ -1,5 +1,4 @@
 """Treeo URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
@@ -14,10 +13,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('ReqAppt.urls')),
-    path('', include('patientLog.urls')),
+    path('', views.patientLog, name='patient_Log-patientLog'),
+
 ]
