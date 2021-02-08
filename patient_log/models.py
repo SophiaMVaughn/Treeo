4069931f10359e9f6dbe7fd977b1c8103e636f62
Nicole <er8015@wayne.edu>
2021-02-07 19:24:57 -0500

    fixed database issues

--- patient_log/models.py
+++ patient_log/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+#blood_category ={
+               #('red blood cells', 'red blood cells'),
+                #('white blood cells', 'white blood cells'),
+                #('platelets', 'platelets'),
+                #('hemoglobin', 'hemoglobin'),
+                #('hematocrit', 'hematocrit'),
+                #}
+
+class PatientLog(models.Model):
+    calories = models.IntegerField(default=0)
+    water = models.DecimalField(max_digits=5, decimal_places=2)
+    blood = models.DecimalField(max_digits=5, decimal_places=2)
+    #date = models.DateTimeField(auto_now_add = True)
+
+
+
+
--- patient_log/views.py
+++ patient_log/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
 
-def patientLog(request):
-    return render(request,'patient_log/patientLog.html ')
+from ReqAppt import models
+from patient_log.models import PatientLog
+from .forms import PatientLogForm
+from decimal import Decimal
+from django.contrib import messages
+
+def patientlog(request):
+    #context = {'form':PatientLogForm()}
+    if request.method =='POST':
+        form = PatientLogForm(request.POST)
+        print("1")
+        if form.is_valid():
+            print("3")
+            print(form.cleaned_data.get('calories'))
+            saverecord=PatientLog()
+            saverecord.calories=form.cleaned_data.get('calories')
+            saverecord.water=form.cleaned_data.get('water')
+            saverecord.blood=form.cleaned_data.get('blood')
+            saverecord.save()
+            #messages.success('your data was saved!')
+            return render(request, 'patient_log/patientLog.html ')
+        else:
+            print("2")
+            return HttpResponseBadRequest()
+    else:
+        form = PatientLogForm()
+        return render(request, 'patient_log/patientLog.html', {"form": form})
+
+
