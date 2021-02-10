c61ea26f1b609ac01b4ef234efda437292e8227a
devnumber4 <copyedinteligence3@gmail.com>
2021-02-09 20:41:38 -0500

    Messaging initial

--- messaging/models.py
+++ messaging/models.py
@@ -2,17 +2,19 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 class message():
-    messageID = models.CharField(max_length = 30,  primary_key=True )#auto generate small increment?
-    sender = models.CharField(max_length = 40)
-    reciever = models.CharField(max_length = 40)
+    #django auto makes a primary key field for id so message.id will get you this
+    #ok so this is interesting do the messages have to be scene from the people after the user is deactivated
+    sender = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    reciever = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     subject = models.CharField(max_length = 70)
     msgbody = models.CharField(max_length = 700)
-    convoID = models.CharField(max_length = 30)#?????????
-    # combine into python date time object
-    send_time = models.CharField(max_length = 30)
-    send_date = models.CharField(max_length = 30)
-    #
-    read_status = models.CharField(max_length = 10)#bool 1 yes 0 no?
-    sender_loc = models.CharField(max_length = 30)#??????????
-    reciever_loc = models.CharField(max_length = 30)#?????????
-    perm_del = models.CharField(max_length = 5)#purpose??????
+    convoID = models.CharField(max_length = 30)# this is for the reply chanin thing in email thing?????????
+    send_time = models.DateTimeField(auto_now_add=True)
+    read_status = models.BooleanField()#1 yes 0 no
+    #how does this work?????????????????????????????
+    # folder that the user has the message in. inbox sent ect
+    sender_loc = models.CharField(max_length = 30)
+    # folder that the user has the message in. inbox sent ect
+    reciever_loc = models.CharField(max_length = 30)
+    # purpose?????? still storeing the mesage in db after the user have deleted it
+    perm_del = models.BooleanField()#1 yes 0 no
