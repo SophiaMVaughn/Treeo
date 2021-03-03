aa11c44546944c882354b086698d41cc32d9acea
George Esho <gi3399@wayne.edu>
2021-03-03 18:31:34 -0500

    Overwriting html files with my changes manually

--- blogsys/forms.py
+++ blogsys/forms.py
@@ -3,6 +3,8 @@ from . import models
 from .models import PostQ
 
 class PostQform(forms.Form):
-    Message = forms.CharField(max_length=200)
+    Message = forms.CharField(max_length=250, widget=forms.Textarea)
 
-
+    def __init__(self, *args, **kwargs):
+        super(PostQform, self).__init__(*args, **kwargs)
+        self.fields['Message'].widget.attrs['style'] = 'width:450px; height:80px;'
