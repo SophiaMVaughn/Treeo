2645701247a8c1fbc2c544919907f550d0a8c953
devnumber4 <copyedinteligence3@gmail.com>
2021-03-03 21:51:41 -0500

    test

--- ReqAppt/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReqapptConfig(AppConfig):
-    name = 'ReqAppt'
--- blogsys/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from . import models
-from .models import PostQ
-
-class PostQform(forms.Form):
-    Message = forms.CharField(max_length=200)
-
-
