067518fb8b05a9084852463ca3a3ef85327056a5
devnumber4 <77869988+Wayne-dev-0912@users.noreply.github.com>
2021-03-12 14:45:14 -0500

    Admin File Upload Download

--- upload_download/forms.py
+++ upload_download/forms.py
@@ -1,5 +1,18 @@
 from django import forms
+from .models import *
+from users_acc.forms import CustomModelChoiceField
+from users_acc.models import *
 
 
-class Fileform(forms.Form):
-    file = forms.FileField()
+class Fileform(forms.ModelForm):
+    #file = forms.FileField()
+
+    class Meta:
+        model = Uploaded_File
+        fields = ['file', 'file_type']
+        labels = {
+            'file': ('File'), 'file_type': ('Type of Document'),
+        }
+
+class AdminProviderFileForm(forms.Form):
+    provider = CustomModelChoiceField(queryset=(Provider.objects.all()), empty_label="(Select a Provider)")
--- upload_download/models.py
+++ upload_download/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.conf import settings
 # Create your models here.
-
+FILE_TYPE_CHOICES = (
+    (1, 'Certificate'),
+    (2, 'License '),
+    (3, 'Other'),
+)
 class Uploaded_File(models.Model):
     usern = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     #how long is max file name and is this the best way to store as var char has to aloctate 100 everytime
     file_name = models.CharField(max_length = 100)
     file = models.FileField(upload_to='uploaded_files')
+    file_type=models.PositiveSmallIntegerField(choices=FILE_TYPE_CHOICES, default=3)
     #make read only
     date_created = models.DateTimeField(auto_now_add=True)
