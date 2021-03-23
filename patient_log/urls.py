d0440156612a56880e7780ea6c292cbfb1c4ee57
gv2479 <71676241+gv2479@users.noreply.github.com>
2021-03-23 17:19:59 -0400

    Update urls.py

--- patient_log/urls.py
+++ patient_log/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from . import views
+
 # from .views import *
 
 urlpatterns = [
     path('', views.patientlog, name='patientlog'),
     # path('Chart', views.chart, name='seechart'),
     # path('charts', Circle.as_view(), name='chart_json'),
+    path('calories/<id>', views.Calories, name='calories'),
     path('log-chart/<id>', views.line_chart_Week, name='log-chart'),
+    path('log-chart1/<id>', views.Mood, name='mood'),
+    path('log-chart2/<id>', views.Sleep, name='sleep'),
+    path('log-chart3/<id>', views.Water, name='water'),
     path('edit_log/<id>', views.edit_log, name='edit_log'),
 ]
