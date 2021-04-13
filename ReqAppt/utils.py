63ca82e88277e0d455dd7044484e1523d86c1c26
Giorgi Nozadze <70528309+fd6100@users.noreply.github.com>
2021-04-12 21:58:26 -0400

    Update utils.py

--- ReqAppt/utils.py
+++ ReqAppt/utils.py
@@ -6,6 +6,10 @@ import random
 import string
 
 
+/*
+	Author: Giorgi Nozadze
+	This method generates the zoom link by using zoom provided credentials, zoom API is ran using wrapper called zoomus
+*/
 
 def generate_zoom(request):
 
@@ -23,7 +27,6 @@ def generate_zoom(request):
     user_list = json.loads(user_list_response.content)
 
 
-    print(user_list_response.content)
 
       # print(json.loads(user_list_response.content))
 
